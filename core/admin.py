89ea192d5241dbafba7b312a9e0d3c98c3b6f023
porublevnik <porublevnik@gmail.com>
2023-06-21 21:44:06 +0300

    docker-compose updated

--- core/admin.py
+++ core/admin.py
@@ -6,7 +6,7 @@ from core.models import User
 
 @admin.register(User)
 class CustomAdmin(UserAdmin):
-    list_display = ('username', 'last_name', 'first_name', 'email')
+    list_display = ('username', 'email', 'first_name', 'last_name')
     readonly_fields = ('last_login', 'date_joined')
     fieldsets = (
         (None, {'fields': ('username', 'password')}),
