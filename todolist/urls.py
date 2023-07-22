567a15bac048a0b3b2cdcc62e52a06dafd63c433
porublevnik <porublevnik@gmail.com>
2023-07-22 14:52:22 +0300

    authorization updated

--- todolist/urls.py
+++ todolist/urls.py
@@ -7,6 +7,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('core/', include('core.urls')),
-    path("oauth/", include("social_django.urls", namespace="social")),
+    path("api/oauth/", include("social_django.urls", namespace="social")),
     path("goals/", include("goals.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
