259d6185be500b02dbb6c4d930a2eb3b2770237b
porublevnik <porublevnik@gmail.com>
2023-08-15 23:15:54 +0300

    permissions added

--- goals/serializers/goal_comments.py
+++ goals/serializers/goal_comments.py
@@ -26,43 +26,6 @@ class GoalCommentCreateSerializer(serializers.ModelSerializer):
         return value
 
 
-# class GoalCommentSerializer(serializers.ModelSerializer):
-#     user = serializers.HiddenField(default=serializers.CurrentUserDefault())
-#
-#     class Meta:
-#         model = GoalComment
-#         fields = "__all__"
-#         read_only_fields = ("id", "created", "updated", "user")
-
-    # def validate_goal(self, value):
-    #
-    #     if not BoardParticipant.objects.filter(
-    #             board_id=value.category.board_id,
-    #             role__in=[BoardParticipant.Role.owner, BoardParticipant.Role.writer],
-    #             user=self.context["request"].user,
-    #     ).exists():
-    #         raise serializers.ValidationError("Вы не являетесь автором этого комментария")
-    #     return value
-
-
-# class GoalCommentDetailSerializer(serializers.ModelSerializer):
-#     user = UserProfileSerializer(read_only=True)
-#
-#     class Meta:
-#         model = GoalComment
-#         fields = "__all__"
-#         read_only_fields = ("id", "created", "updated", "user")
-
-    # def validate_goal(self, value):
-    #
-    #     if not BoardParticipant.objects.filter(
-    #             board_id=value.category.board_id,
-    #             role__in=[BoardParticipant.Role.owner, BoardParticipant.Role.writer],
-    #             user=self.context["request"].user,
-    #     ).exists():
-    #         raise serializers.ValidationError("Вы не являетесь автором этого комментария")
-    #     return value
-
 class GoalCommentSerializer(GoalCommentCreateSerializer):
     user = UserProfileSerializer(read_only=True)
     goal = serializers.PrimaryKeyRelatedField(read_only=True)
